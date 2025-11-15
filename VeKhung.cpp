55a685524f13726bdd592c815395e720777b74bf
24521503_BuiNgocNhuQuynh <24521503@gm.uit.edu.vn>
2025-11-15 22:16:03 +0700

    Ve-Khung
    Code phần VeKhung và gọi ở hàm main

---
+++ VeKhung.cpp
@@ -0,0 +1,15 @@
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
+
+void VeKhung() {
+    for (int i = MINX; i <= MAXX; i++) {
+        for (int j = MINY; j <= MAXY; j++) {
+            if (i == MINX || i == MAXX || j == MINY || j == MAXY) {
+                gotoxy(i, j);
+                printf("*");
+            }
+        }
+    }
+}
